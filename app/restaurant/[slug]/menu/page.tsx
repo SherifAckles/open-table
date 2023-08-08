cdc45226bbedd26bd87419332315014f39fbdedf
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-08-08 11:03:58 -0500

    clean up the code

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import NavBar from './../../../components/NavBar';
 
 export default function Menu() {
   return (
     <main className='bg-gray-100 min-h-screen w-screen'>
       <main className='max-w-screen-2xl m-auto bg-white'>
         {/* NAVBAR */}
-        <nav className='bg-white p-2 flex justify-between'>
-          <Link href='/' passHref>
-            <span className='font-bold text-gray-700 text-2xl'>OpenTable</span>
-          </Link>
-          <div>
-            <div className='flex'>
-              <button className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'>
-                Sign in
-              </button>
-              <button className='border p-1 px-4 rounded'>Sign up</button>
-            </div>
-          </div>
-        </nav>
+        <NavBar />
         {/* NAVBAR */}
         {/* HEADER */}
         <div className='h-96 overflow-hidden'>
