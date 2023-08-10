08ca1ae0cdde6303d52686cc1fa8fe1e6720596b
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-08-09 20:58:56 -0500

    refactor the code

---
+++ app/restaurant/[slug]/components/MenuComponent.tsx
@@ -0,0 +1,19 @@
+
+import MenuCard from './MenuCard';
+
+export default function MenuComponent() {
+  return (
+    <main className='bg-white mt-5'>
+      <div>
+        <div className='mt-4 pb-1 mb-1'>
+          <h1 className='font-bold text-4xl'>Menu</h1>
+        </div>
+        <div className='flex flex-wrap justify-between'>
+          {/* MENU CARD */}
+          <MenuCard/>
+          
+        </div>
+      </div>
+    </main>
+  );
+}
