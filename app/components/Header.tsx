ebd17cb9fe7c305100c482723071769448a68f70
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-08-15 18:33:40 -0500

    filtered restaurants by price,cuisine

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,20 @@
-
-
 import SearchBar from "./SearchBar";
+
+const backgroundImageUrl =
+  "https://opentable.github.io/design-tokens/static/handoverBanner-4bc0b987ea5b879a3095152bb0bd555f.png";
+
 export default function Header() {
   return (
-    <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2'>
-      <div className='text-center mt-10'>
-        <h1 className='text-white text-5xl font-bold mb-2'>
-          Find your table for any occasion
-        </h1>
-        <SearchBar />
+    <div
+      className='h-64 bg-cover bg-center'
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
+      <div className='bg-opacity-40 bg-black h-full p-2'>
+        <div className='text-center mt-10'>
+          <h1 className='text-white text-5xl font-bold mb-2'>
+            Find your table for any occasion
+          </h1>
+          <SearchBar />
+        </div>
       </div>
     </div>
   );
