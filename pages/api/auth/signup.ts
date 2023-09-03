1b9a3d558117bd8fca6795b1f4caa9d13e43e321
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-09-02 21:44:58 -0500

    validate the user inputs

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,14 +1,62 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   // EXTRACT THE BODY
-  const body = req.body;
+  const { firstName, lastName, email, phone, city, password } = req.body;
+
+  const errors: string[] = [];
   const responseData = {
-    hello: body,
+    hello: "body",
   };
+
+  // Validate the user inputs
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "First name is invalid",
+    },
+    {
+      valid: validator.isLength(lastName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "Last name is invalid",
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isMobilePhone(phone),
+      errorMessage: "Phone is invalid",
+    },
+    {
+      valid: validator.isLength(city, { min: 1 }),
+      errorMessage: "City is invalid",
+    },
+    {
+      valid: validator.isStrongPassword(password),
+      errorMessage: "Please create a strong password",
+    },
+  ];
+
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage);
+    }
+  });
+
+  if (errors.length) {
+    return res.status(400).json({ errorMessage: errors[0] });
+  }
+
   if (req.method === "POST") {
     res.status(200).json(responseData);
   }
