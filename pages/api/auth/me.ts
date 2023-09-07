5e29b37b4852874c21b443a67f49e902832dce11
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-09-07 10:43:50 -0500

    test token 2

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -10,7 +10,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse) {
     //if the header in there(the key header)
     if (!bearerToken) {
       return res.status(401).json({
-        errorMessage: "Unauthorized request",
+        errorMessage: "Unauthorized request (no bearer token) ",
       });
     }
     //split the token from the bearerToken token
