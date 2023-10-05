b6b07f87a4cdb60bc85e958c0a530f6e146a021c
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-10-05 10:12:33 -0500

    finish adding middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
+export async function middleware(req: NextRequest, res: NextResponse) {
+ 
 
+  //get the header from the NextApiRequest
+  const bearerToken = req.headers.get("authorization")as string;
+  //if the header in there(the key header)
+  if (!bearerToken) {
+   return new NextResponse(
+      JSON.stringify({errorMessage:"Unauthorized request"}),{status:401}
+    )
+    };
+    
+  //split the token from the bearerToken token
+  const token = bearerToken.split(" ")[1];
 
-export async function middleware(req:NextRequest,res:NextResponse) {
-  
+  if (!token) {
+   return new NextResponse(
+     JSON.stringify({ errorMessage: "Unauthorized request" }),
+     { status: 401 }
+   );
+  }
+  //if we have token let's verify it
+  //GRAB THE SECRET TOKEN TO IDENTIFY THE TOKEN
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  try {
+    await jose.jwtVerify(token, secret);
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ errorMessage: "Unauthorized request" }),
+      { status: 401 }
+    );
+  }
 }
+//just protect the auth route not all pages
+export const config = {
+  matcher: ["/api/auth/me"],
+};
--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,61 +1,37 @@
 //identify users with json Token
 
 import { NextApiRequest, NextApiResponse } from "next";
-import * as jose from 'jose'
+
 import jwt from 'jsonwebtoken'
 import { PrismaClient } from "@prisma/client";
 
 const prisma=new PrismaClient()
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse) {
+  //split the token from the bearerToken token
+  const bearerToken = req.headers["authorization"] as string;
+  const token = bearerToken.split(" ")[1];
 
-//get the header from the NextApiRequest
-    const bearerToken = req.headers['authorization'] as string
-    //if the header in there(the key header)
-    if (!bearerToken) {
-      return res.status(401).json({
-        errorMessage: "Unauthorized request (no bearer token) ",
-      });
-    }
-    //split the token from the bearerToken token
-    const token = bearerToken.split(" ")[1]
-
-    if (!token) {
-      return res.status(401).json({
-        errorMessage: "Unauthorized request",
-      });
-    }
-//if we have token let's verify it
-    //GRAB THE SECRET TOKEN TO IDENTIFY THE TOKEN
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET) 
-    try {
-       await jose.jwtVerify(token,secret) 
-    } catch (error) {
-        return res.status(401).json({
-        errorMessage: "Unauthorized request",
-      })
-  }
-  
   //decode the verified token
-  const payload = jwt.decode(token) as { email: string }
+  const payload = jwt.decode(token) as { email: string };
   if (!payload.email) {
-      return res.status(401).json({
-        errorMessage: "Unauthorized request",
-      });
+    return res.status(401).json({
+      errorMessage: "Unauthorized request",
+    });
   }
-  const user=await prisma.user.findUnique({
-    where:{
-      email:payload.email
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
     },
     //just select to return certain values
     select: {
       id: true,
-      first_name:true,
-      last_name:true,
-      email:true,
-      city:true,
-      phone:true
-    }
-  })
-   return res.json({user})
+      first_name: true,
+      last_name: true,
+      email: true,
+      city: true,
+      phone: true,
+    },
+  });
+  return res.json({ user });
 }
